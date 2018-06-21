4036a87177916746504c632c3ee6c3886d8edfb1
Sylvain Carlioz <sylvain.carlioz@gmail.com>
2018-06-21 23:52:56 +0200

    Temporary FIX for finding plugins folder

--- src/pluginmanager.cpp
+++ src/pluginmanager.cpp
@@ -312,7 +312,8 @@ bool PlugInManager::LoadAllPlugIns(const wxString &plugin_dir, bool load_enabled
     //  Enable the compatibility dialogs if requested, and has not been already done once.
     m_benable_blackdialog = b_enable_blackdialog && !m_benable_blackdialog_done;
     
-    m_plugin_location = plugin_dir;
+    //m_plugin_location = plugin_dir;
+    m_plugin_location = "/Users/sc/Documents/Code/OpenCPN/build/plugins";
 
     wxString msg(_T("PlugInManager searching for PlugIns in location "));
     msg += m_plugin_location;
